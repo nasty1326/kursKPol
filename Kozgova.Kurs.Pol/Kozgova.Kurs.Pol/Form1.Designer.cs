fdee87e3213bbebd8b6998e2e67c0992096e9f55
nasty1326 <92375864+nasty1326@users.noreply.github.com>
2022-09-01 14:20:05 +0700

    Add files via upload

---
+++ Kozgova.Kurs.Pol/Kozgova.Kurs.Pol/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿
+namespace Kozgova.Kurs.Pol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numOutMut = new System.Windows.Forms.NumericUpDown();
+            this.label13 = new System.Windows.Forms.Label();
+            this.numOutCross = new System.Windows.Forms.NumericUpDown();
+            this.label12 = new System.Windows.Forms.Label();
+            this.numOutTurn = new System.Windows.Forms.NumericUpDown();
+            this.label11 = new System.Windows.Forms.Label();
+            this.numOutPop = new System.Windows.Forms.NumericUpDown();
+            this.label10 = new System.Windows.Forms.Label();
+            this.numOutGen = new System.Windows.Forms.NumericUpDown();
+            this.label9 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.numOutMut)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numOutCross)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numOutTurn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numOutPop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numOutGen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(92, 254);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 77);
+            this.button2.TabIndex = 24;
+            this.button2.Text = "Старт";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numOutMut
+            // 
+            this.numOutMut.DecimalPlaces = 2;
+            this.numOutMut.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numOutMut.Location = new System.Drawing.Point(211, 189);
+            this.numOutMut.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numOutMut.Name = "numOutMut";
+            this.numOutMut.Size = new System.Drawing.Size(86, 22);
+            this.numOutMut.TabIndex = 34;
+            this.numOutMut.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(24, 28);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(124, 17);
+            this.label13.TabIndex = 25;
+            this.label13.Text = "Число поколений";
+            // 
+            // numOutCross
+            // 
+            this.numOutCross.DecimalPlaces = 2;
+            this.numOutCross.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numOutCross.Location = new System.Drawing.Point(211, 149);
+            this.numOutCross.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numOutCross.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numOutCross.Name = "numOutCross";
+            this.numOutCross.Size = new System.Drawing.Size(86, 22);
+            this.numOutCross.TabIndex = 33;
+            this.numOutCross.Value = new decimal(new int[] {
+            9,
+            0,
+            0,
+            65536});
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(24, 71);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(132, 17);
+            this.label12.TabIndex = 26;
+            this.label12.Text = "Размер популяции";
+            // 
+            // numOutTurn
+            // 
+            this.numOutTurn.Location = new System.Drawing.Point(211, 110);
+            this.numOutTurn.Maximum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numOutTurn.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numOutTurn.Name = "numOutTurn";
+            this.numOutTurn.Size = new System.Drawing.Size(86, 22);
+            this.numOutTurn.TabIndex = 32;
+            this.numOutTurn.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(24, 112);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(115, 17);
+            this.label11.TabIndex = 27;
+            this.label11.Text = "Размер турнира";
+            // 
+            // numOutPop
+            // 
+            this.numOutPop.Location = new System.Drawing.Point(211, 69);
+            this.numOutPop.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numOutPop.Name = "numOutPop";
+            this.numOutPop.Size = new System.Drawing.Size(86, 22);
+            this.numOutPop.TabIndex = 31;
+            this.numOutPop.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(24, 151);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(185, 17);
+            this.label10.TabIndex = 28;
+            this.label10.Text = "Вероятность скрещивания";
+            // 
+            // numOutGen
+            // 
+            this.numOutGen.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numOutGen.Location = new System.Drawing.Point(211, 28);
+            this.numOutGen.Maximum = new decimal(new int[] {
+            150,
+            0,
+            0,
+            0});
+            this.numOutGen.Name = "numOutGen";
+            this.numOutGen.Size = new System.Drawing.Size(86, 22);
+            this.numOutGen.TabIndex = 30;
+            this.numOutGen.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(24, 191);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(152, 17);
+            this.label9.TabIndex = 29;
+            this.label9.Text = "Вероятность мутации";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(361, 30);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.Lime;
+            series1.Legend = "Legend1";
+            series1.LegendText = "Min в поколении";
+            series1.Name = "Series1";
+            series2.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.DarkOrange;
+            series2.Legend = "Legend1";
+            series2.LegendText = "Среднее в поколении";
+            series2.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(887, 300);
+            this.chart1.TabIndex = 35;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1352, 557);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numOutMut);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.numOutCross);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.numOutTurn);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.numOutPop);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.numOutGen);
+            this.Controls.Add(this.label9);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numOutMut)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numOutCross)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numOutTurn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numOutPop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numOutGen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numOutMut;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.NumericUpDown numOutCross;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.NumericUpDown numOutTurn;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.NumericUpDown numOutPop;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.NumericUpDown numOutGen;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Kozgova.Kurs.Pol/Kozgova.Kurs.Pol/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kozgova.Kurs.Pol
+{
+    public partial class Form1 : Form
+    {
+        double min0OutPol; // максимальное значение выживаемости (близость к ответу)
+        double sredOutPol; // среднее значение выживаемости
+        int numberGenOutPol; // число поколений
+        int numberPopOutPol; // размер популяции
+        int numberTurnOutPol; // размер турнира 
+        double pcrossOutPol; // вероятность скрещивания
+        double pmutationOutPol; // вероятность мутации
+        int genNowOutPol; // текущее поколение
+        int sizeNewOutPol; // число популяции после скрещивания
+
+        const int lchrom = 2; // число генов
+        const int maxpop = 500; // максимальный размер популяции
+        const double xmax = 2;
+        const double xmin = 1;
+
+        public class individualOutPol //особь без пола
+        {
+            public double[] chrom { get; set; } = new double[lchrom];
+            public double fitness { get; set; }  // коэффициент выживаемости (значение функции) 
+            public individualOutPol()
+            {
+                for (int i = 0; i < lchrom; i++)
+                {
+                    chrom[i] = -1;
+                }
+                fitness = -1;
+            }
+        }
+
+        // Создаем массивы для беcполового скрещивания
+        individualOutPol[] oldpopOutPol = new individualOutPol[maxpop]; // массив для старого поколения полового скрещивания
+        individualOutPol[] newpopOutPol = new individualOutPol[maxpop]; // массив для нового поколения полового скрещивания
+        individualOutPol[] intpopOutPol = new individualOutPol[maxpop]; // массив для сортировки поколения для полового скрещивания
+
+        //основная функция Растригина f(x1, x2) = 20 + x1*x1 + x2*x2 − 10(cos 2πx1 + cos2πx2) 
+        double objfunc(double x1, double x2)
+        {
+           return (20 + x1 * x1 + x2 * x2 - 10 * (Math.Cos(2 * Math.PI * x1) + Math.Cos(2 * Math.PI * x2)));
+           
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void initpopOutPol()
+        {
+            Random rnd = new Random();
+            for (int i=0; i< numberPopOutPol; i++)
+            {
+                
+                oldpopOutPol[i].chrom[0] = 0 + rnd.NextDouble()*2;                
+                oldpopOutPol[i].chrom[1] = 0 + rnd.NextDouble()*2;
+                oldpopOutPol[i].fitness = objfunc(oldpopOutPol[i].chrom[0], oldpopOutPol[i].chrom[1]);
+            }
+        }
+
+        public void statisticsOutPol(individualOutPol[] pop)
+        {
+            min0OutPol = pop[0].fitness;
+            sredOutPol = pop[0].fitness;
+            for (int i=1; i < sizeNewOutPol; i++)
+            {
+                sredOutPol += pop[i].fitness;
+                if (pop[i].fitness < min0OutPol)
+                {
+                    min0OutPol = pop[i].fitness;
+                }
+            }
+            sredOutPol = sredOutPol / sizeNewOutPol;
+            chart1.Series[0].Points.AddXY(genNowOutPol, min0OutPol);
+            chart1.Series[1].Points.AddXY(genNowOutPol, sredOutPol);
+            
+        }
+
+
+        public double mutation (double fit)
+        {
+            Random rnd = new Random();
+            double p = rnd.NextDouble();
+            if (p<pmutationOutPol)
+            {
+                fit = fit - rnd.NextDouble()/10; //0,1-0,05=0,05   0-0,05=-0,05
+            }
+            return fit;
+        }
+
+        public void crossoverOutPol()
+        {
+            double p;
+            
+            Random rnd = new Random();
+            for (int i=0; i <= sizeNewOutPol; i++)
+            {
+             
+                p = rnd.NextDouble();
+                if (p<pcrossOutPol)
+                {
+                    newpopOutPol[i].chrom[0] = mutation(oldpopOutPol[i].chrom[0]);
+                    newpopOutPol[i].chrom[1] = mutation(oldpopOutPol[i].chrom[1]);
+                    newpopOutPol[i].fitness = objfunc(newpopOutPol[i].chrom[0], newpopOutPol[i].chrom[1]);
+                }
+                else
+                {
+                    newpopOutPol[i] = oldpopOutPol[i];
+                    
+                }
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < maxpop; i++)
+            {
+                oldpopOutPol[i] = new individualOutPol();//старое поколение популяции
+                newpopOutPol[i] = new individualOutPol();// новое поколение популяции
+                intpopOutPol[i] = new individualOutPol();// полонение для сортировки 
+            }
+
+            numberGenOutPol = (int)numOutGen.Value;
+            numberPopOutPol = (int)numOutPop.Value;
+            numberTurnOutPol = (int)numOutTurn.Value;
+            pcrossOutPol = (double)numOutCross.Value;
+            pmutationOutPol = (double)numOutMut.Value;
+            sizeNewOutPol = numberPopOutPol;
+
+            initpopOutPol(); //создаем первое поколение популяции
+           
+            genNowOutPol = 1;
+            statisticsOutPol(oldpopOutPol);
+
+            do
+            {
+                genNowOutPol++;
+
+               
+                crossoverOutPol();
+                
+                statisticsOutPol(newpopOutPol); // находим больший коэффициент выживаемости у поколения
+
+                for (int i = 0; i < maxpop; i++) // новое поколение превращаем в старое 
+                { oldpopOutPol[i] = newpopOutPol[i]; }
+                
+
+            } while (genNowOutPol < numberGenOutPol); // сколько раз было задано 
+
+            
+        }
+    }
+}
